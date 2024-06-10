29b5c92a1811c2b920c396ef32bfb74e50fcfca1
MAS-80 <andrej.makruschin@yandex.ru>
2024-06-10 23:33:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+m=str("Hallo world")
+print(len(m))
+first=int(9)
+second=int(7)
+summa=first+second
+diff=first-second
+print(summa,diff)
+first=int(9)
+second=int(7)
+third=int(5)
+mean=(first+second+third)/3
+print(mean)
+first_string=str('Вторник')
+second_string=str('Понедельник, ')
+print(second_string+first_string)
+a=int(4)
+b=int(6)
+c=int(8)
+f=(a*b)+(a*c)
+g=(f**3)/2
+print(g)
